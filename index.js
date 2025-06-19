e2e53ec92cac611f49c22ab33f4783a7f0c0e6a9
johann.silang <johann.silang@gmail.com>
2025-06-19 13:01:47 +0800

    fixed port

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const port = process.env.PORT || 4000;
 require("dotenv").config();
 
 //Routes Middleware
@@ -15,10 +16,10 @@ mongoose.connection.once("open", () => console.log("Connected to MongoDB Atlas."
 app.use("/workouts", workoutRoutes);
 app.use("/users", userRoutes);
 
-// if(require.main === module){
-// 	app.listen(process.env.PORT || 4000, () => {
-// 	    console.log(`API is now online on port ${ process.env.PORT || 4000 }.`)
-// 	});
-// }
+if(require.main === module){
+	app.listen(port, '0.0.0.0', () => {
+	  console.log(`Server is listening on port ${port}.`);
+	});
+}
 
 module.exports = {app,mongoose};
