6e8b1e29127c0b5e59671110ccb6c887b96726f9
johann.silang <johann.silang@gmail.com>
2025-06-19 12:44:00 +0800

    fitnessAPI

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ mongoose.connection.once("open", () => console.log("Connected to MongoDB Atlas."
 app.use("/workouts", workoutRoutes);
 app.use("/users", userRoutes);
 
-if(require.main === module){
-	app.listen(process.env.PORT || 4000, () => {
-	    console.log(`API is now online on port ${ process.env.PORT || 4000 }.`)
-	});
-}
+// if(require.main === module){
+// 	app.listen(process.env.PORT || 4000, () => {
+// 	    console.log(`API is now online on port ${ process.env.PORT || 4000 }.`)
+// 	});
+// }
 
 module.exports = {app,mongoose};
